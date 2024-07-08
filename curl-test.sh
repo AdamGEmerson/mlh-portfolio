301cc5e287a966edb4659bf530c41ed67d90a375
Adam G. Emerson <adamgemerson@gmail.com>
2024-07-08 14:29:34 +0200

    Added curl-test.sh

---
+++ curl-test.sh
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:8000/api/timeline_post -d 'name=Adam&content=This is a test of the api endpoints using the curl script&email=adamgemerson@gmail.com'
+
+curl -X GET http://localhost:8000/api/timeline_post
